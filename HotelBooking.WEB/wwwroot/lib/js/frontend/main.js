1ab7a5d8118671dd9e5ffe2f1f4a93adae15a830
Trung Nguyen <xuantrung48@gmail.com>
2020-08-12 15:51:38 +0700

    Trung_Move Lib

---
+++ HotelBooking.WEB/wwwroot/lib/js/frontend/main.js
@@ -0,0 +1,34 @@
+﻿digitGrouping = function (price) {
+    return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
+
+showRoomTypes = function () {
+    $.ajax({
+        url: "/RoomType/GetAll",
+        method: "GET",
+        dataType: "json",
+        success: function (data) {
+            $.each(data.result, function (i, v) {
+                $.ajax({
+                    url: `/RoomTypeImage/GetByRoomTypeId/${v.roomTypeId}`,
+                    method: "GET",
+                    dataType: "json",
+                    success: function (imgs) {
+                        console.log(imgs.result[0].imageData);
+                        $('#roomTypes').append(
+                            `<div class="col-lg-3 col-md-6 col-sm-6 p-0">
+                                <div class="home__room__item set-bg" data-setbg="${imgs.result[0].imageData}">
+                                    <div class="home__room__title">
+                                        <h4>${v.name}</h4>
+                                        <h2>${digitGrouping(v.defaultPrice)}₫<span>/đêm</span></h2>
+                                    </div>
+                                    <a href="#">Đặt ngay</a>
+                                </div>
+                            </div>`
+                        );
+                    }
+                });
+            });
+        }
+    });
+}
