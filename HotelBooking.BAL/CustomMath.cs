48842a415a691236341162fea4a5e2cca01a4c64
lktram1@gmail.com <lktram1@gmail.com>
2020-08-13 09:42:50 +0700

    Tram update RoomTypeTest

---
+++ HotelBooking.BAL/CustomMath.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Query.Internal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelBooking.BAL
+{
+    public class CustomMath
+    {
+        //Create 4 function
+        // Additional, Subtract, Multiple, Division
+
+        public int Additional (int number1, int number2)
+        {
+            return number1 + number2;
+        }
+        public int  Subtraction (int number1, int number2)
+        {
+            return number1 - number2;
+        }
+        public int Multiply(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+        public float Division(float number1, float number2)
+        {
+            //if (number2 == 0)
+            //    return "Divison by zero";
+            //return (number1 / number2).ToString();
+            try
+            {
+                return number1 / number2;
+            }
+            catch (Exception e)
+            {
+                return float.MinValue;
+            }
+        }
+    }
+
+}
