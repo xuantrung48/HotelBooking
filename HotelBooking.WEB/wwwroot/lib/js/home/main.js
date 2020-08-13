27aaab8d719a8db7550b4afb6f272ab42c0ae000
Trung Nguyen <xuantrung48@gmail.com>
2020-08-13 20:30:40 +0700

    Trung-Show RoomTypes on HomePage

--- HotelBooking.WEB/wwwroot/lib/js/home/main.js
+++ HotelBooking.WEB/wwwroot/lib/js/home/main.js
@@ -4,91 +4,66 @@
 
 showRoomTypes = function () {
     $.ajax({
-        url: "/RoomType/GetAll",
+        url: "/Promotion/GetAll",
         method: "GET",
         dataType: "json",
-        success: function (data) {
-            console.log(data);
-            /*for (let i = 0; i < data.result.length; i++) {
-                console.log(i);
-                $.ajax({
-                    url: `/RoomTypeImage/GetByRoomTypeId/${data.result[i].roomTypeId}`,
-                    method: "GET",
-                    dataType: "json",
-                    success: function (imgs) {
-                        console.log(i);
-                        console.log(imgs);
-                        if (i = 0) {
+        success: function (promotions) {
+            $.ajax({
+                url: "/RoomType/GetAllWithImagesAndFacilities",
+                method: "GET",
+                dataType: "json",
+                success: function (data) {
+                    var now = new Date();
+                    for (let i = 0; i < data.result.length; i++) {
+                        if (i == 0) {
                             $('.carousel-indicators').append(
                                 `<li data-target="#roomTypes" data-slide-to="${i}" class="active"></li>`
                             );
-                        } else {
-                            $('.carousel-indicators').append(
-                                `<li data-target="#roomTypes" data-slide-to="${i}"></li>`
-                            );
-                        }
-
-                        if (i = 0) {
                             $('.carousel-inner').append(
                                 `<div class="carousel-item active">
-                                    <img src="${imgs.result[0].imageData}" alt="${data.result[i].name}" width="1100" height="500">
-                                </div>`
-                            );
-                        } else {
-                            $('.carousel-inner').append(
-                                `<div class="carousel-item">
-                                    <img src="${imgs.result[0].imageData}" alt="${data.result[i].name}" width="1100" height="500">
-                                </div>`
-                            );
-                        }
-                    }
-                });
-            }*/
-            /*$.each(data.result, function (i, v) {
-                console.log(i);
-                $.ajax({
-                    url: `/RoomTypeImage/GetByRoomTypeId/${v.roomTypeId}`,
-                    method: "GET",
-                    dataType: "json",
-                    success: function (imgs) {
-                        if (i = 0) {
-                            $('.carousel-indicators').append(
-                                `<li data-target="#roomTypes" data-slide-to="${i}" class="active"></li>`
-                            );
-                        } else {
-                            $('.carousel-indicators').append(
-                                `<li data-target="#roomTypes" data-slide-to="${i}"></li>`
-                            );
-                        }
-
-                        if (i = 0) {
-                            $('.carousel-inner').append(
-                                `<div class="carousel-item active">
-                                    <img src="${imgs.result[0].imageData}" alt="${v.name}" width="1100" height="500">
-                                </div>`
-                            );
-                        } else {
-                            $('.carousel-inner').append(
-                                `<div class="carousel-item">
-                                    <img src="${imgs.result[0].imageData}" alt="${v.name}" width="1100" height="500">
-                                </div>`
-                            );
-                        }
-
-                        *//*$('#roomTypes').append(
-                            `<div class="col-lg-3 col-md-6 col-sm-6 p-0">
-                                <div class="home__room__item set-bg" style="background-image: url('${imgs.result[0].imageData}');">
-                                    <div class="home__room__title">
-                                        <h4>${v.name}</h4>
-                                        <h2>${digitGrouping(v.defaultPrice)}₫<span>/đêm</span></h2>
+                                    <img src="${data.result[i].images[0].imageData}" alt="${data.result[i].name}" width="100%" height="auto">
+                                    <div class="carousel-caption">
+                                        <h3 class="text-success">${data.result[i].name}</h3>
+                                        <p class="text-warning" id="price${data.result[i].roomTypeId}">${digitGrouping(data.result[i].defaultPrice)}₫<span>/đêm</span></p>
+                                        <a href="/#bookroom">Đặt ngay</a>
                                     </div>
-                                    <a href="#">Đặt ngay</a>
-                                </div>
-                            </div>`
-                        );*//*
+                                </div>`
+                                    );
+                                } else {
+                                    $('.carousel-indicators').append(
+                                        `<li data-target="#roomTypes" data-slide-to="${i}"></li>`
+                                    );
+                                    $('.carousel-inner').append(
+                                        `<div class="carousel-item">
+                                    <img src="${data.result[i].images[0].imageData}" alt="${data.result[i].name}" width="100%" height="auto">
+                                    <div class="carousel-caption">
+                                        <h3 class="text-success">${data.result[i].name}</h3>
+                                        <p class="text-warning" id="price${data.result[i].roomTypeId}">${digitGrouping(data.result[i].defaultPrice)}₫<span>/đêm</span></p>
+                                        <a href="/#bookroom">Đặt ngay</a>
+                                    </div>
+                                </div>`
+                            );
+                        }
+                        $.ajax({
+                            url: `/PromotionApply/GetByRoomTypeId/${data.result[i].roomTypeId}`,
+                            method: "GET",
+                            dataType: "json",
+                            success: function (roomTypePromotions) {
+                                for (let j = 0; j < promotions.result.length; j++) {
+                                    for (let k = 0; k < roomTypePromotions.result.length; k++) {
+                                        if (promotions.result[j].promotionId == roomTypePromotions.result[k].promotionId && now >= new Date(promotions.result[j].startDate) && now <= new Date(promotions.result[j].endDate)) {
+                                            $(`#price${data.result[i].roomTypeId}`).empty();
+                                            $(`#price${data.result[i].roomTypeId}`).append(
+                                                `<small><del class="text-danger">${digitGrouping(data.result[i].defaultPrice)}₫</del></small> ${digitGrouping(data.result[i].defaultPrice * promotions.result[j].discountRates)}₫`
+                                            )
+                                        }
+                                    }
+                                }
+                            }
+                        })
                     }
-                });
-            });*/
+                }
+            });
         }
-    });
+    })
 }
